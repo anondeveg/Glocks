37367d396dc66dd0477ea5500d8ce8363cbacc88
Anondeveg <anondeveg@gmail.com>
2026-06-04 06:47:21 +0300

    feat(RPN) reverse polish notation visitor class

---
+++ cmd/rpn.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type Rpn struct{}
+
+func (r *Rpn) visitLiteral(l *Literal) any {
+	if l.value == nil {
+		return "nil"
+	}
+	return fmt.Sprintf("%v ", l.value)
+}
+
+func (r *Rpn) visitBinary(b *Binary) any {
+	return fmt.Sprintf("%v %v %v", b.left.accept(r), b.right.accept(r), b.operator.lexeme)
+}
+
+func (r *Rpn) visitUnary(u *Unary) any {
+	return fmt.Sprintf("%v %v", u.right.accept(r), u.operator.lexeme)
+}
+
+func (r *Rpn) visitGrouping(g *Grouping) any {
+	return fmt.Sprintf("%v", g.groupedExpression.accept(r))
+}
